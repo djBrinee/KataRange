c673817427d9087fab6b6583e9fbdfb53175d1c2
idroj1 <mejiajordi@yahoo.com>
2022-06-15 19:13:03 -0400

    alexa

--- test_KataRange.py
+++ test_KataRange.py
@@ -1,5 +1,9 @@
 import pytest
 from KataRange import RangeClass
 
+c1 = RangeClass(1, 6)
+
 def test_RangeContains():
-    assert KangeClass.Contains(2) == True
+    assert c1.Contains(2) == True
+
+
